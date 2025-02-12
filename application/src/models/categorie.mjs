5c24af3bcd885abc994ace6c6b6cb013249e6bb2
antoineFabr <163823977+antoineFabr@users.noreply.github.com>
2025-02-12 10:40:34 +0100

    🦺 ajout du model écrivain

--- application/src/models/categorie.mjs
+++ application/src/models/categorie.mjs
@@ -1,28 +1,36 @@
 import { sequelize } from "sequelize";
 
 const categorieModel = (sequelize, DataTypes) => {
-  return sequelize.define("utilisateur", {
-    categorie_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    nom: {
-      type: DataTypes.STRING,
-      allowNul: false,
-      validate: {
-        is: {
-          args: /^[A-Za-z\s]*$/,
-          msg: "Seules les lettres et les espaces sont autorisés",
-        },
-        notEmpty: {
-          msg: "le nom de la categorie ne peut pas etre vide",
-        },
-        notNull: {
-          msg: "le nom de la categorie est obligatoire",
+  return sequelize.define(
+    "utilisateur",
+    {
+      categorie_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      nom: {
+        type: DataTypes.STRING,
+        allowNul: false,
+        validate: {
+          is: {
+            args: /^[A-Za-z\s]*$/,
+            msg: "Seules les lettres et les espaces sont autorisés",
+          },
+          notEmpty: {
+            msg: "le nom de la categorie ne peut pas etre vide",
+          },
+          notNull: {
+            msg: "le nom de la categorie est obligatoire",
+          },
         },
       },
     },
-  });
+    {
+      timestamps: true,
+      createdAt: "created",
+      updatedAt: false,
+    }
+  );
 };
 export { categorieModel };
---
+++ application/src/models/ecrivain.mjs
@@ -0,0 +1,52 @@
+import { sequelize } from "sequelize";
+
+const ecrivainModel = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "Ecrivain",
+    {
+      ecrivain_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      nom: {
+        type: DataTypes.STRING,
+        allowNul: false,
+        validate: {
+          is: {
+            args: /^[A-Za-z\s\-]*$/,
+            msg: "Seules les lettres et les espaces sont autorisés et les -",
+          },
+          notEmpty: {
+            msg: "le nom ne peut pas etre vide",
+          },
+          notNull: {
+            msg: "le nom est obligatoire",
+          },
+        },
+      },
+      prenom: {
+        type: DataTypes.STRING,
+        allowNul: false,
+        validate: {
+          is: {
+            args: /^[A-Za-z\s\-]*$/,
+            msg: "Seules les lettres et les espaces sont autorisés et les -",
+          },
+          notEmpty: {
+            msg: "le prenom ne peut pas etre vide",
+          },
+          notNull: {
+            msg: "le prenom est obligatoire",
+          },
+        },
+      },
+    },
+    {
+      timestamps: true,
+      createdAt: "created",
+      updatedAt: false,
+    }
+  );
+};
+export { ecrivainModel };
