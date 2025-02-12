b1e8fac1d45cbddc1373073ed69ed82399e68470
antoineFabr <163823977+antoineFabr@users.noreply.github.com>
2025-02-12 10:45:08 +0100

    🦺 ajout du model editeur.mjs

---
+++ application/src/models/editeur.mjs
@@ -0,0 +1,36 @@
+import { sequelize } from "sequelize";
+
+const editeurModel = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "Editeur",
+    {
+      editeur_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      nom: {
+        type: DataTypes.STRING,
+        allowNul: false,
+        validate: {
+          is: {
+            args: /^[A-Za-z\s\-]*$/,
+            msg: "Seules les lettres et les espaces sont autorisés et les -",
+          },
+          notEmpty: {
+            msg: "le nom ne peut pas etre vide",
+          },
+          notNull: {
+            msg: "le nom est obligatoire",
+          },
+        },
+      },
+    },
+    {
+      timestamps: true,
+      createdAt: "created",
+      updatedAt: false,
+    }
+  );
+};
+export { editeurModel };
