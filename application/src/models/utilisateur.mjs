3d0761b3c134cec057db8bb75cfc5da16df4a0d9
antoineFabr <163823977+antoineFabr@users.noreply.github.com>
2025-02-12 08:22:44 +0100

    model utilisateur ✔🎂

--- application/src/models/utilisateur.mjs
+++ application/src/models/utilisateur.mjs
@@ -1,18 +1,40 @@
 import { sequelize } from "sequelize";
 
 const utilisateurModel = (sequelize, DataTypes) => {
-  return sequelize.define("utilisateur", {
-    utilisateur_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    pseudo: {
-      type: DataTypes.STRING,
-      allowNul: false,
-      unique: {
-        msg: "ce pseudo est deja pris",
+  return sequelize.define(
+    "utilisateur",
+    {
+      utilisateur_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      pseudo: {
+        type: DataTypes.STRING,
+        allowNul: false,
+        unique: {
+          msg: "ce pseudo est deja pris",
+        },
+        validate: {
+          is: {
+            args: /^[A-Za-z0-9\s\-\.\,]*$/,
+            msg: "Seules les lettres, les espaces et les - sont autorisés",
+          },
+          notEmpty: {
+            msg: "le pseudo ne peut pas etre vide",
+          },
+          notNull: {
+            msg: "le pseudo est obligatoire",
+          },
+        },
       },
     },
-  });
+    {
+      timestamps: true,
+      createdAt: "created",
+      updatedAt: false,
+    }
+  );
 };
+
+export { utilisateurModel };
