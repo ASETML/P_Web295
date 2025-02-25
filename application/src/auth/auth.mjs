bf6209b0855f17d33d341a55d7966d5189ece2a9
antoineFabr <163823977+antoineFabr@users.noreply.github.com>
2025-02-25 15:50:24 +0100

    :construction_worker: ajout d'un systeme de token jwt

---
+++ application/src/auth/auth.mjs
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import { privateKey } from "../config.mjs";
+
+const auth = (req, res, next) => {
+  const authorizationHeader = req.header.authorization;
+  if (!authorizationHeader) {
+    const message = `vous n'avez pas fourni de jeton d'authentification`;
+    return res.status(401).json({ message });
+  } else {
+    const token = authorizationHeader.split(" ")[1];
+    const decodedToken = jwt.verify(
+      token,
+      privateKey,
+      (error, decodedToken) => {
+        if (error) {
+          const message = `l'utilisateur n'est pas autorisé a cette ressource`;
+          return res.status(401).json({ message, data: error });
+        }
+        const userId = decodedToken.userId;
+        if (req.body.utilisateurId && req.body.utilisateurId !== userId) {
+          const message = `l'identifiant de l'utilisateur est invalide`;
+          res.status(401).json({ message });
+        } else {
+          next();
+        }
+      }
+    );
+  }
+};
+export { auth };
