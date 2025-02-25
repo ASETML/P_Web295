785e909ac05cd238e1e74cf9752674770b03fa07
ASETML <alban.segalen@eduvaud.ch>
2025-02-25 15:11:13 +0100

    ✏️ Remplacer allowNul par AllowNull dans le modèle éditeur

--- application/src/models/editeur.mjs
+++ application/src/models/editeur.mjs
@@ -1,5 +1,3 @@
-import { sequelize } from "sequelize";
-
 const editeurModel = (sequelize, DataTypes) => {
   return sequelize.define(
     "Editeur",
@@ -11,7 +9,7 @@ const editeurModel = (sequelize, DataTypes) => {
       },
       nom: {
         type: DataTypes.STRING,
-        allowNul: false,
+        allowNull: false,
         validate: {
           is: {
             args: /^[A-Za-z\s\-]*$/,
