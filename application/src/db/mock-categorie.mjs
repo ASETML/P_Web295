ca400896a4529fc3a21105807296fdd806c4c7de
pn41htu <pn41htu@eduvaud.ch>
2025-03-04 16:58:10 +0100

    📁  🎊  Data ajoutées ET RegEx OK

--- application/src/db/mock-categorie.mjs
+++ application/src/db/mock-categorie.mjs
@@ -7,4 +7,44 @@ export const categories = [
     id: 2,
     nom: "Guides",
   },
+  {
+    id: 3,
+    nom: "Programmation",
+  },
+  {
+    id: 4,
+    nom: "Développement Web",
+  },
+  {
+    id: 5,
+    nom: "Sécurité Informatique",
+  },
+  {
+    id: 6,
+    nom: "Finance et Économie",
+  },
+  {
+    id: 7,
+    nom: "Intelligence Artificielle",
+  },
+  {
+    id: 8,
+    nom: "Design et UX/UI",
+  },
+  {
+    id: 9,
+    nom: "Développement Personnel",
+  },
+  {
+    id: 10,
+    nom: "Science et Technologie",
+  },
+  {
+    id: 11,
+    nom: "Littérature Classique",
+  },
+  {
+    id: 12,
+    nom: "Histoire et Géopolitique",
+  },
 ];
