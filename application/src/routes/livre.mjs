62123c208b5eafd17df8ebd8021ae52a42bb67ec
pn41htu <pn41htu@eduvaud.ch>
2025-03-04 17:19:11 +0100

    🔍 👷 Recherche livre : OK 🎊

--- application/src/routes/livre.mjs
+++ application/src/routes/livre.mjs
@@ -13,58 +13,39 @@ import { success } from "./helper.mjs";
 const livreRouter = express();
 
 //Listes des livres + recherche - marche pas
-livreRouter.get("/", auth, (req, res) => {
-  let recherche = req.query.search;
-  let categorie = req.query.cat;
+livreRouter.get("/", auth, async (req, res) => {
+  let recherche = req.query.search || "%"; // Si vide, ça prend la valeur "%"
+  let categorie = req.query.cat || "%"; // Si vide, ça prend la valeur "%"
   let booksPreview = [];
-  let preview = {
-    livre_id: null,
-    titre: null,
-    annee_edition: null,
-    ecrivain_nom: null,
-    ecrivain_prenom: null,
-    categorie_nom: null,
-  };
-  if (!categorie) {
-    categorie = "%";
-  }
-  if (!recherche) {
-    recherche = "%";
-  }
 
-  Livre.findAll({
-    where: {
-      titre: { [Op.like]: `%${recherche}%` },
-      categorie_fk: { [Op.like]: categorie },
-    },
-  }).then((books) => {
+  try {
+    const books = await Livre.findAll({
+      where: {
+        titre: { [Op.like]: `%${recherche}%` },
+        categorie_fk: { [Op.like]: categorie },
+      },
+    });
+
     for (const book of books) {
-      preview.livre_id = book.livre_id;
-      preview.titre = book.titre;
-      preview.annee_edition = book.annee_edition;
+      // Récupérer l'écrivain et la catégorie pour chaque livre
+      const ecrivain = await Ecrivain.findByPk(book.ecrivain_fk);
+      const categorieRecup = await Categorie.findByPk(book.categorie_fk);
 
-      Ecrivain.findByPk(book.ecrivain_fk).then((ecrivain) => {
-        preview.ecrivain_nom = ecrivain.nom;
-        preview.ecrivain_prenom = ecrivain.prenom;
+      // Construire l'objet preview pour chaque livre
+      booksPreview.push({
+        livre_id: book.livre_id,
+        titre: book.titre,
+        annee_edition: book.annee_edition,
+        ecrivain_nom: ecrivain ? ecrivain.nom : null,
+        ecrivain_prenom: ecrivain ? ecrivain.prenom : null,
+        categorie_nom: categorieRecup ? categorieRecup.nom : null,
       });
-
-      Categorie.findByPk(book.categorie_fk).then((categorie) => {
-        preview.categorie_nom = categorie.nom;
-      });
-      console.log(preview);
-
-      booksPreview.push(preview);
-      preview = {
-        livre_id: null,
-        titre: null,
-        annee_edition: null,
-        ecrivain_nom: null,
-        ecrivain_prenom: null,
-        categorie_nom: null,
-      };
     }
     res.json(success("La liste des livres à bien été récupérée", booksPreview));
-  });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
 });
 // ============================================================================
 // ========================== > Détails d'un livres < =========================
