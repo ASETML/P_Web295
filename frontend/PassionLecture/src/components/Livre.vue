05f08b9c5a08c841d6a2e72b12156e06be40ad46
antoineFabr <163823977+antoineFabr@users.noreply.github.com>
2025-04-09 10:34:35 +0200

    👷 ajout du component Livre

---
+++ frontend/PassionLecture/src/components/Livre.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="livre">
+    <img :src="livre.image" alt="" />
+    <h2>{{ livre.titre }}</h2>
+    <p>{{ livre.description }}</p>
+    <p>{{ livre.editeur }} - {{ livre.annee }}</p>
+  </div>
+</template>
+<script setup>
+defineProps({
+  livre: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
