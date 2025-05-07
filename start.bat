a993531e78b89c7c1e14e78993c087ac7d652175
ASETML <alban.segalen@eduvaud.ch>
2025-05-07 08:13:09 +0200

    🔨 Ouverture de VSCode dans le script de démarrage

--- start.bat
+++ start.bat
@@ -1,8 +1,8 @@
 @echo off
+cmd /k "code . && exit"
 echo "Demarrage des serveurs"
 start cmd /k "cd backend/application && npm i && npm start"
 echo "Demarrage du backend"
 start cmd /k "cd frontend/PassionLecture && npm i && npm run dev"
 echo "Demarrage du frontend"
-pause
 exit
