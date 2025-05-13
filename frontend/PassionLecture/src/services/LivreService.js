c13dde406029ff3c4ddad359d6d5e4fab6994ac2
ASETML <alban.segalen@eduvaud.ch>
2025-05-13 15:18:54 +0200

    ♻️ Les services importent axios depuis apiClient.js

--- frontend/PassionLecture/src/services/LivreService.js
+++ frontend/PassionLecture/src/services/LivreService.js
@@ -1,9 +1,4 @@
-import axios from 'axios'
-
-const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  withCredentials: true,
-})
+import apiClient from './ApiClient'
 
 export default {
   getLivreFromCategorie(id) {
