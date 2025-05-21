d04baaaddcdb25f738c39275e2ad681a53dc9613
ASETML <alban.segalen@eduvaud.ch>
2025-05-21 08:21:34 +0200

    🔨 Le script de démarrage ouvre: le repo, le projets, gitmoji et l'application

--- start.bat
+++ start.bat
@@ -1,10 +1,23 @@
 @echo off
 cmd /k "code . && exit"
 echo "Demarrage des serveurs"
-start cmd /k "cd backend/application && npm i && npm start"
 echo "Demarrage du backend"
-start cmd /k "cd frontend/PassionLecture && npm i && npm run dev"
-echo "Demarrage du frontend"
+start cmd /k "cd backend/application && npm i && npm start"
 
+echo "Demarrage du frontend"
+start cmd /k "cd frontend/PassionLecture && npm i && npm run dev"
+
+echo "Ouverture du repo et du projet"
+start https://github.com/ASETML/P_Web295 && start https://github.com/users/ASETML/projects/7
+
+echo "Ouverture du CDC"
 start ./frontend/E-P_Web294-CDC.pdf
-exit
+
+echo "Ouverture de Gitmoji"
+start https://gitmoji.dev
+
+echo "Ouverture de l'application"
+start http://localhost:5173
+
+echo "Prêt à travailler"
+pause && exit
