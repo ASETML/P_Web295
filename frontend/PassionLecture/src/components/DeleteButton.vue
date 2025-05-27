c6c10e6fb933fa1cf3197c3e26f768c92e25d6cb
ASETML <alban.segalen@eduvaud.ch>
2025-05-27 17:03:35 +0200

    ✨ On peut supprimer un livre depuis la page de compte

---
+++ frontend/PassionLecture/src/components/DeleteButton.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { computed } from 'vue'
+import LivreService from '@/services/LivreService'
+
+const props = defineProps(['livre'])
+
+const emit = defineEmits(['delete']) //Evènements
+
+const livre = computed(() => props.livre)
+
+const DeleteBook = () => {
+  const confirmation = confirm(
+    'Etes vous sûr que vous voulez supprimmer le livre: ' + livre.value.titre,
+  )
+  if (confirmation) {
+    LivreService.delLivre(livre.value.livre_id).then((res) => {
+      emit('delete')
+    })
+  }
+}
+</script>
+<template>
+  <button @click="DeleteBook">Supprimmer</button>
+</template>
+<style scoped></style>
