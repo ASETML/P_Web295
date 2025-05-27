e79c48301828786650c387994d97cc2edf08b347
ASETML <alban.segalen@eduvaud.ch>
2025-05-27 14:00:11 +0200

    🦺 Message d'erreur quand il n'y a pas de commentaire

--- frontend/PassionLecture/src/components/CommentaireForm.vue
+++ frontend/PassionLecture/src/components/CommentaireForm.vue
@@ -8,20 +8,32 @@ const props = defineProps(['id'])
 const id = computed(() => props.id)
 
 const commentaire = ref(null)
+const error = ref(null)
+
 const comment = async () => {
-  await CommentaireService.postCommentaires(id.value, commentaire.value).then((res) => {
-    commentaire.value = null
-    emit('commenter')
-  })
+  await CommentaireService.postCommentaires(id.value, commentaire.value)
+    .then((res) => {
+      commentaire.value = null
+      emit('commenter')
+    })
+    .catch((err) => {
+      if (err.response.status === 400) {
+        error.value = true
+      }
+    })
 }
 </script>
 
 <template>
-  <form @submit.prevent="comment">
-    <label for="commentaire">Commenter ce livre</label>
-    <textarea id="commentaire" v-model="commentaire"></textarea>
-    <input type="submit" />
-  </form>
+  <section>
+    <h3>Commenter ce livre</h3>
+    <form @submit.prevent="comment">
+      <label for="commentaire">Votre commentaire</label>
+      <textarea id="commentaire" v-model="commentaire"></textarea>
+      <input type="submit" />
+    </form>
+    <label class="error" v-if="error">Le commentaire est obligatoire</label>
+  </section>
 </template>
 
 <style scoped>
@@ -29,10 +41,12 @@ form {
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: left;
 }
 label {
   margin-top: 2%;
-  font-size: larger;
+}
+.error {
+  color: red;
 }
 </style>
